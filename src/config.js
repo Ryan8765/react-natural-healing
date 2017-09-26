f76578e649488ab21636d44f5a052d6fd6ca6ac9
Ryan8765 <rmhaas2211@gmail.com>
2017-09-26 18:01:47 -0500

    updated base_server_url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-export const BASE_SERVER_URL = "http://localhost:3090";
+//export const BASE_SERVER_URL = "http://localhost:3090";
+export const BASE_SERVER_URL = "https://mighty-retreat-10959.herokuapp.com/";
+
 
 
