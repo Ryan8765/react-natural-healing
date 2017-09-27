fdbcdee34be70c02e12ee2e7576430527c1b0c64
Ryan8765 <rmhaas2211@gmail.com>
2017-09-27 17:11:54 -0500

    Updated base_url

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,9 +7,9 @@ import { setLocalUser_h } from '../helpers/auth';
 
 //HISTORY is used to redirect user. 
 export const signinUser =  (email, password, history) => {
+	alert( "base URL ************** " + `${BASE_SERVER_URL}/signin` );
 
 	return function( dispatch ) {
-		alert( "base URL ************** " + `${BASE_SERVER_URL}/signin` );
 		//submit email/password to the server
 		axios({
 			method: 'post',
